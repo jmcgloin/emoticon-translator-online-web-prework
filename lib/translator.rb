46df841c1efffc568fa7029346c94a545416cff3
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 04:21:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,14 @@ require 'pry'
 def load_library(lib)
   emotes = YAML.load_file(lib)
   emotes
+  output = {}
+  output["get_emoticon"] = {}
+  output["get_meaning"] = {}
+  emotes.each do
+    |entry|
+    
   binding.pry
+  end
 end
 
 def get_japanese_emoticon
